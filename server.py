f8a21bdae0e7a7f0fb1c1529c2b4df501c1d528e
ynw2 <yvonne_wang_3@sfu.ca>
2022-11-28 16:31:43 -0800

    added some more message scenarios

--- server.py
+++ server.py
@@ -2,12 +2,15 @@
 from socket import *
 
 # Specify Server Port
-serverHost = 'localhost'
+# serverHost = 'localhost'
+serverHost = ''
+
 serverPort = 8000
 
 # Create TCP welcoming socket
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSocket.settimeout(10)
 
 # Bind the server port to the socket
 serverSocket.bind((serverHost,serverPort))
@@ -19,37 +22,46 @@ print ('Listening on port ', serverPort, '...')
 while True: # Loop forever
     # Server waits on accept for incoming requests.
     # New socket created on return
-    connectionSocket, addr = serverSocket.accept()
-     
+    try:
+        connectionSocket, addr = serverSocket.accept()
+    except timeout:
+        continue
+
     # Read from socket (but not address as in UDP)
     request = connectionSocket.recv(1024).decode()
     print(request)
+
     if (request==''):
-        continue
+        reply = 'HTTP/1.0 408 Request Timed Out\n\n408 Request Timed Out'
 
-    # Get HTTP header
-    header = request.split('\n')    
-    filename = header[0].split()[1]
-    
+    else:
+        # Get HTTP header
+        header = request.split('\n')    
+        filename = header[0].split()[1]
 
-    if (filename == '/'):
-        filename = 'test.html'
+        if (filename == '/'):
+            filename = 'test.html'
+        else:
+            filename = filename[1:]
 
-    try:
-        f = open(filename)
-        content = f.read()
-        f.close()
+        try:
+            f = open(filename)
+            content = f.read()
+            f.close()
 
-        reply = 'HTTP/1.0 200 OK\n\n' + content
+            reply = 'HTTP/1.0 200 OK\n\n' + content
 
-    except FileNotFoundError:
-        reply = 'HTTP/1.0 404 NOT FOUND\n\n404 File not found'
+        except FileNotFoundError:
+            reply = 'HTTP/1.0 404 Not Found\n\n404 Not Found'
 
+        # Send the reply
+        try:
+            connectionSocket.sendall(reply.encode())
 
-    # Send the reply
-    connectionSocket.sendall(reply.encode())
-     
-    # Close connectiion to client (but not welcoming socket)
+        except timeout:
+            reply = 'HTTP/1.0 408 Request Timed Out\n\n408 Request Timed Out'
+
+    # Close connection to client (but not welcoming socket)
     connectionSocket.close()
 
 serverSocket.close()
